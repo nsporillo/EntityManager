a2436de245e54c639eccf5e1855ad0be16f76cbc
Nick Porillo <milkywayz@mail.com>
2013-07-10 22:30:00 -0400

    Fix AROOBE when using just /em , also refractor

--- src/net/milkycraft/em/commands/CommandHandler.java
+++ src/net/milkycraft/em/commands/CommandHandler.java
@@ -8,7 +8,7 @@ import java.util.Map;
 
 import net.milkycraft.em.EntityManager;
 
-import org.bukkit.ChatColor;
+import static org.bukkit.ChatColor.*;
 import org.bukkit.command.CommandSender;
 
 /**
@@ -16,37 +16,32 @@ import org.bukkit.command.CommandSender;
  */
 public class CommandHandler {
 
-	@SuppressWarnings("unused")
-	private final EntityManager plugin;
-	private final Map<String, Command> commands = new HashMap<String, Command>();
+	private Map<String, Command> cmds = new HashMap<String, Command>();
 
-	public CommandHandler(final EntityManager plugin) {
-		this.plugin = plugin;
-		commands.put("reload", new ReloadCommand(plugin));
+	public CommandHandler(EntityManager plugin) {
+		cmds.put("reload", new ReloadCommand(plugin));
 	}
 
-	public void runCommand(final CommandSender sender, final String label,
-			final String[] args) {
-		sender.sendMessage(label);
-		if (this.commands.get(args[0].toLowerCase()) == null) {
-			sender.sendMessage(ChatColor.GREEN + "===" + ChatColor.GOLD
-					+ " EntityManager Help " + ChatColor.GREEN + "===");
-			for (final Command cmd : this.commands.values()) {
-				if (cmd.checkPermission(sender)) {
-					cmd.showHelp(sender, label);
-				}
+	public void runCommand(CommandSender s, String l, String[] a) {
+		if (a.length == 0 || this.cmds.get(a[0].toLowerCase()) == null) {
+			this.showHelp(s, l);
+			return;
+		}
+		List<String> args = new ArrayList<String>(Arrays.asList(a));
+		Command cmd = this.cmds.get(args.remove(0).toLowerCase());
+		if (args.size() < cmd.getRequiredArgs()) {
+			cmd.showHelp(s, l);
+			return;
+		}
+		cmd.runCommand(s, args);
+	}
+
+	private void showHelp(CommandSender s, String l) {
+		s.sendMessage(GREEN + "===" + GOLD + " EntityManager Help " + GREEN + "===");
+		for (Command cmd : this.cmds.values()) {
+			if (cmd.checkPermission(s)) {
+				cmd.showHelp(s, l);
 			}
-			return;
 		}
-		final List<String> arguments = new ArrayList<String>(
-				Arrays.asList(args));
-		final Command cmd = this.commands
-				.get(arguments.remove(0).toLowerCase());
-		if (arguments.size() < cmd.getRequiredArgs()) {
-			cmd.showHelp(sender, label);
-			return;
-		}
-		cmd.runCommand(sender, arguments);
 	}
-
 }
