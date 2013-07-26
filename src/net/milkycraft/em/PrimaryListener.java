41a47bb06edaacb76c0d0619745a656136bb5580
Nick Porillo <milkywayz@mail.com>
2013-07-25 23:42:31 -0400

    Fix anvil blocking, add keeping items on player death, add support for blocking baby / villager mobs! and blocking them from spawning with armor

--- src/net/milkycraft/em/PrimaryListener.java
+++ src/net/milkycraft/em/PrimaryListener.java
@@ -1,21 +1,28 @@
 package net.milkycraft.em;
 
 import static net.milkycraft.em.config.Option.*;
-import static org.bukkit.event.block.Action.*;
-import static org.bukkit.entity.EntityType.*;
+import static org.bukkit.entity.EntityType.fromId;
+import static org.bukkit.event.block.Action.RIGHT_CLICK_AIR;
+import static org.bukkit.event.block.Action.RIGHT_CLICK_BLOCK;
 
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
 import java.util.Set;
-
+import net.milkycraft.em.config.Type;
 import net.milkycraft.em.config.WorldConfiguration;
 
+import org.bukkit.Bukkit;
 import org.bukkit.Material;
+import org.bukkit.entity.Ageable;
 import org.bukkit.entity.Creature;
 import org.bukkit.entity.EnderPearl;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.entity.Projectile;
+import org.bukkit.entity.Zombie;
 import org.bukkit.event.Event.Result;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
@@ -31,16 +38,21 @@ import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.entity.PotionSplashEvent;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.inventory.InventoryType;
+import org.bukkit.event.inventory.InventoryType.SlotType;
 import org.bukkit.event.player.PlayerInteractEntityEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.player.PlayerRespawnEvent;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.potion.Potion;
 
 public class PrimaryListener extends Utility implements Listener {
 
 	private Set<Entity> mobs = new HashSet<Entity>();
+	private Map<String, List<ItemStack>> drops = new HashMap<String, List<ItemStack>>();
 	private String[] p = { "entitymanager.interact.trade",
 			"entitymanager.interact.shoot", "entitymanager.interact.enchant",
-			"entitymanager.interact.anvil", "entitymanager.death.keepexp" };
+			"entitymanager.interact.anvil", "entitymanager.death.keepexp",
+			"entitymanager.death.keepitems" };
 
 	public PrimaryListener(EntityManager manager) {
 		super(manager);
@@ -72,7 +84,7 @@ public class PrimaryListener extends Utility implements Listener {
 				}
 			} else if (e.getItem().getType().equals(Material.MONSTER_EGG)) {
 				EntityType type = fromId(e.getItem().getDurability());
-				if (c.get(3).contains(type.toString())) {
+				if (c.getMap(1).containsKey(type.toString())) {
 					if (!b(pl, "entitymanager.spawn." + str)) {
 						if (b(e.getClickedBlock())) {
 							e.setUseItemInHand(Result.DENY);
@@ -187,8 +199,8 @@ public class PrimaryListener extends Utility implements Listener {
 
 	@EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
 	public void onAnvilEnchant(InventoryClickEvent e) {
-		if (e.getInventory().getType().equals(InventoryType.ANVIL)) {
-			if (e.getSlot() == 9) {
+		if (e.getInventory().getType() == InventoryType.ANVIL) {
+			if (e.getSlotType() == SlotType.RESULT) {
 				if (e.getWhoClicked() instanceof Player) {
 					Player pl = (Player) e.getWhoClicked();
 					WorldConfiguration conf = a(pl.getWorld());
@@ -209,20 +221,54 @@ public class PrimaryListener extends Utility implements Listener {
 
 	@EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
 	public void onSpawn(CreatureSpawnEvent e) {
-		WorldConfiguration conf = a(e.getEntity().getWorld());
-		if (conf.get(4).contains(e.getEntityType().toString())
-				|| conf.get(5).contains(e.getSpawnReason().toString())) {
+		WorldConfiguration conf = a(e.getEntity());
+		String et = e.getEntityType().toString();
+		if (conf.getMap(2).containsKey(et)) {
+			if (e.getEntity() instanceof Ageable) {
+				Ageable a = (Ageable) e.getEntity();
+				if (!a.isAdult()) {
+					if (conf.getMap(2).get(et) == Type.BABY) {
+						e.setCancelled(true);
+					} else {
+						return;
+					}
+				} else {
+					return;
+				}
+			} else if (e.getEntity() instanceof Zombie) {
+				Zombie z = (Zombie) e.getEntity();
+				if (z.isBaby() || z.isVillager()) {
+					if (conf.getMap(2).get(et) == Type.BOTH) {
+						e.setCancelled(true);
+					}
+				} else if (z.isBaby() && !z.isVillager()) {
+					if (conf.getMap(2).get(et) == Type.BABY) {
+						e.setCancelled(true);
+					}
+				} else if (z.isVillager() && !z.isBaby()) {
+					System.out.println("Villager!");
+					if (conf.getMap(2).get(et) == Type.VILLAGER) {
+						e.setCancelled(true);
+					}
+				}
+			} else if (conf.getMap(2).get(et) == Type.ALL) {
+				e.setCancelled(true);
+			}
+		} else if (conf.get(5).contains(e.getSpawnReason().toString())) {
 			e.setCancelled(true);
-			return;
 		} else if (e.getSpawnReason().equals(SpawnReason.SPAWNER)) {
 			mobs.add(e.getEntity());
+		}
+		if (conf.get(NOMOBARMOR)) {
+			e.getEntity().getEquipment().setArmorContents(null);
+			e.getEntity().getEquipment().getItemInHand().setTypeId(0);
 		}
 	}
 
 	@EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
 	public void onDeath(EntityDeathEvent e) {
 		if (e.getEntity() instanceof Creature) {
-			WorldConfiguration conf = a(e.getEntity().getWorld());
+			WorldConfiguration conf = a(e.getEntity());
 			if (mobs.contains(e.getEntity())) {
 				if (conf.get(NOEXP)) {
 					e.setDroppedExp(0);
@@ -244,11 +290,11 @@ public class PrimaryListener extends Utility implements Listener {
 	public void onPotionSplash(PotionSplashEvent e) {
 		if (e.getEntity().getShooter() instanceof Player) {
 			Player p = (Player) e.getEntity().getShooter();
-			WorldConfiguration conf = a(p.getWorld());
 			Potion b = fromDamage(e.getPotion().getItem().getDurability());
 			if (b(p, "entitymanager.interact.potion." + b.getNameId())) {
 				return;
 			}
+			WorldConfiguration conf = a(p.getWorld());
 			if (conf.get(6).contains(b.getNameId())) {
 				e.setCancelled(true);
 				al(conf, "Player " + p.getName() + " tried to use an " + c(b)
@@ -262,10 +308,38 @@ public class PrimaryListener extends Utility implements Listener {
 
 	@EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
 	public void onPlayerDeath(PlayerDeathEvent e) {
-		WorldConfiguration conf = a(e.getEntity().getWorld());
-		if (b(e.getEntity(), p[4]) || conf.get(PDEATHEXP)) {
+		Player pl = e.getEntity();
+		WorldConfiguration conf = a(pl.getWorld());
+		if (b(pl, p[5]) || conf.get(PDEATHITEMS)) {
+			drops.put(pl.getName(), e.getDrops());
+		}
+		if (b(pl, p[4]) || conf.get(PDEATHEXP)) {
 			e.setKeepLevel(true);
 			e.setDroppedExp(0);
+		}
+	}
+
+	@EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
+	public void onRespawn(PlayerRespawnEvent e) {
+		final Player p = e.getPlayer();
+		if (drops.containsKey(p.getName())) {
+			Bukkit.getScheduler().runTaskLater(getHandle(), new Runnable() {
+
+				@Override
+				public void run() {
+					WorldConfiguration wc = a(p.getWorld());
+					al(wc, "Player " + p.getName()
+							+ " respawned with their items");
+					al(wc, p, "&6Your items were returned after death!");
+					for (ItemStack is : drops.get(p.getName())) {
+						if (is != null) {
+							p.getInventory().addItem(is);
+						}
+					}
+					drops.remove(p.getName());
+				}
+
+			}, 10L);
 		}
 	}
 
