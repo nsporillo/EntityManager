ed006d4c7ccbb28aa2b592f580353fa4674d0c51
Nick Porillo <milkywayz@mail.com>
2013-07-27 01:34:18 -0400

    Major re-arrangement, fixed blocking items

--- src/net/milkycraft/em/AuxiliaryListener.java
+++ src/net/milkycraft/em/AuxiliaryListener.java
@@ -1,9 +1,6 @@
 package net.milkycraft.em;
 
-import static net.milkycraft.em.config.Option.FISHING;
-import static net.milkycraft.em.config.Option.LIGHTNING;
-import static net.milkycraft.em.config.Option.RAIN;
-import static net.milkycraft.em.config.Option.THUNDER;
+import static net.milkycraft.em.config.Option.*;
 import static org.bukkit.Material.EXP_BOTTLE;
 import static org.bukkit.Material.POTION;
 import static org.bukkit.Material.SNOW_BALL;
@@ -11,43 +8,104 @@ import static org.bukkit.entity.EntityType.FISHING_HOOK;
 import static org.bukkit.entity.EntityType.SNOWBALL;
 import static org.bukkit.entity.EntityType.SPLASH_POTION;
 import static org.bukkit.entity.EntityType.THROWN_EXP_BOTTLE;
+import static org.bukkit.entity.EntityType.fromId;
+import static org.bukkit.event.block.Action.RIGHT_CLICK_AIR;
+import static org.bukkit.event.block.Action.RIGHT_CLICK_BLOCK;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import net.milkycraft.em.config.Type;
 import net.milkycraft.em.config.WorldConfiguration;
 
+import org.bukkit.DyeColor;
 import org.bukkit.Material;
+import org.bukkit.entity.Ageable;
+import org.bukkit.entity.Creature;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
+import org.bukkit.entity.Sheep;
+import org.bukkit.entity.Zombie;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.Event.Result;
+import org.bukkit.event.entity.CreatureSpawnEvent;
+import org.bukkit.event.entity.EntityDeathEvent;
 import org.bukkit.event.entity.ProjectileLaunchEvent;
-import org.bukkit.event.weather.LightningStrikeEvent;
-import org.bukkit.event.weather.ThunderChangeEvent;
-import org.bukkit.event.weather.WeatherChangeEvent;
+import org.bukkit.event.entity.CreatureSpawnEvent.SpawnReason;
+import org.bukkit.event.player.PlayerInteractEntityEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.potion.Potion;
 
 public class AuxiliaryListener extends Utility implements Listener {
 
+	private Set<Entity> mobs = new HashSet<Entity>();
 	public AuxiliaryListener(EntityManager manager) {
 		super(manager);
 		super.register(this);
 	}
 
 	@EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
-	public void onLightning(LightningStrikeEvent e) {
-		e.setCancelled(a(e.getWorld()).get(LIGHTNING));
-	}
-
-	@EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
-	public void onThunderChange(ThunderChangeEvent e) {
-		boolean b = e.toThunderState();
-		e.setCancelled(b ? a(e.getWorld()).get(THUNDER) : false);
-	}
-
-	@EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
-	public void onWeatherChange(WeatherChangeEvent e) {
-		boolean b = e.toWeatherState();
-		e.setCancelled(b ? a(e.getWorld()).get(RAIN) : false);
+	public void onSpawn(CreatureSpawnEvent e) {
+		WorldConfiguration conf = a(e.getEntity());
+		String et = e.getEntityType().toString();
+		if (conf.has(et)) {
+			if (e.getEntity() instanceof Sheep) {
+				Sheep sheep = (Sheep) e.getEntity();
+				byte color = sheep.getColor().getWoolData();
+				if (!sheep.isAdult()) {
+					if (conf.block(et, Type.BABY, color)) {
+						e.setCancelled(true);
+						return;
+					} else {
+						return;
+					}
+				}
+				if (conf.block(et, color)) {
+					e.setCancelled(true);
+				}
+				return;
+			} else if (e.getEntity() instanceof Ageable) {
+				Ageable a = (Ageable) e.getEntity();
+				if (!a.isAdult()) {
+					if (conf.block(et, Type.BABY)) {
+						e.setCancelled(true);
+					} else {
+						return;
+					}
+				} else {
+					return;
+				}
+			} else if (e.getEntity() instanceof Zombie) {
+				Zombie z = (Zombie) e.getEntity();
+				  if (z.isBaby() && !z.isVillager()) {
+					if (conf.block(et, Type.BABY)) {
+						e.setCancelled(true);
+					}
+				} else if (z.isVillager() && !z.isBaby()) {
+					if (conf.block(et, Type.VILLAGER)) {					
+						e.setCancelled(true);
+					}
+				} else if (z.isBaby() || z.isVillager()) {
+					if (conf.block(et, Type.BOTH)) {						
+						e.setCancelled(true);
+					}
+				}
+				return;
+			}
+			e.setCancelled(true);
+		} else if (conf.get(5).contains(e.getSpawnReason().toString())) {
+			e.setCancelled(true);
+		} else if (e.getSpawnReason().equals(SpawnReason.SPAWNER)) {
+			mobs.add(e.getEntity());
+		}
+		if (conf.get(NOMOBARMOR)) {
+			e.getEntity().getEquipment().setArmorContents(null);
+			e.getEntity().getEquipment().getItemInHand().setTypeId(0);
+		}
 	}
 
 	@EventHandler(priority = EventPriority.HIGH, ignoreCancelled = true)
@@ -115,6 +173,148 @@ public class AuxiliaryListener extends Utility implements Listener {
 				e.setCancelled(true);
 				al(conf, "Player " + p.getName() + " tried to fish");
 				al(conf, p, "&cYou don't have permission to fish.");
+			}
+		}
+	}
+	
+	@EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
+	public void onDeath(EntityDeathEvent e) {
+		if (e.getEntity() instanceof Creature) {
+			WorldConfiguration conf = a(e.getEntity());
+			if (mobs.contains(e.getEntity())) {
+				if (conf.get(NOEXP)) {
+					e.setDroppedExp(0);
+				} else if (conf.get(NODROPS)) {
+					e.getDrops().clear();
+				}
+				mobs.remove(e.getEntity());
+			}
+			if (!conf.get(EDEATHEXP)) {
+				e.setDroppedExp(0);
+			}
+			if (!conf.get(EDEATHDROPS)) {
+				e.getDrops().clear();
+			}
+		}
+	}
+	
+	@EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
+	public void onTrade(PlayerInteractEntityEvent e) {
+		if (e.getRightClicked() instanceof org.bukkit.entity.Villager) {
+			Player pl = e.getPlayer();
+			WorldConfiguration conf = get(pl.getWorld().getName());
+			if (conf.get(TRADING) && !b(pl, p[0])) {
+				e.setCancelled(true);
+				al(conf, "Player " + pl.getName() + " tried to trade ");
+				al(conf, pl, "&cYou don't have permission to trade.");
+			}
+		} else if (e.getRightClicked() instanceof Sheep) {
+			WorldConfiguration conf = a(e.getRightClicked());
+			String et = e.getRightClicked().getType().toString();
+			Player p = e.getPlayer();
+			ItemStack is = p.getItemInHand();
+			if (conf.has(et) && is.getTypeId() == 351) {	
+				DyeColor dc = DyeColor.getByDyeData((byte) is.getDurability());	
+				Sheep sheep = (Sheep) e.getRightClicked();
+				if (!sheep.isAdult()) {
+					if (conf.block(et, Type.BABY, dc.getWoolData())) {
+						e.setCancelled(true);
+						al(conf, p, "&cThat color of sheep is blocked");
+						al(conf, "Player " + p.getName() + " tried to dye a sheep " + dc.toString().toLowerCase());
+						update(sheep);
+						return;
+					} else {
+						return;
+					}
+				}
+				if (conf.block(et, dc.getWoolData())) {
+					e.setCancelled(true);
+					al(conf, p, "&cThat color of sheep is blocked");
+					al(conf, "Player " + p.getName() + " tried to dye a sheep " + dc.toString().toLowerCase());
+					update(sheep);
+				}
+				return;
+			}
+		}
+	}
+	
+	@EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
+	public void onInteraction(PlayerInteractEvent e) {
+		if (!e.hasItem())
+			return;
+		if (e.getAction() == RIGHT_CLICK_AIR
+				|| e.getAction() == RIGHT_CLICK_BLOCK) {
+			WorldConfiguration c = get(e.getPlayer().getWorld().getName());
+			Player pl = e.getPlayer();
+			String str = e.getItem().getType().toString().toLowerCase();
+			if (e.getItem().getType().equals(Material.FIREWORK)) {
+				if (c.get(FIREWORKS) && !b(pl, "entitymanager.interact." + str)) {
+					if (b(e.getClickedBlock())) {
+						e.setUseItemInHand(Result.DENY);
+						return;
+					} else {
+						e.setUseItemInHand(Result.DENY);
+						e.setCancelled(true);
+						al(c, "Player " + pl.getName()
+								+ " tried to use a firework.");
+						al(c, pl,
+								"&cYou don't have permission to use fireworks.");
+					}
+				}
+			} else if (e.getItem().getType().equals(Material.MONSTER_EGG)) {
+				EntityType type = fromId(e.getItem().getDurability());
+				if (c.get(3).contains(type.toString())) {
+					if (!b(pl, "entitymanager.spawn." + str)) {
+						if (b(e.getClickedBlock())) {
+							e.setUseItemInHand(Result.DENY);
+							return;
+						} else {
+							e.setUseItemInHand(Result.DENY);
+							e.setCancelled(true);
+							al(c, "Player " + pl.getName()
+									+ " tried to spawn a " + str);
+							al(c, pl, "&cYou don't have permission to spawn "
+									+ str + "s.");
+						}
+					}
+				}
+			} else if (c.get(2).contains(e.getItem().getType().toString())) {
+				if (e.getItem().getType() == Material.POTION) {
+					Potion b = fromDamage(e.getItem().getDurability());
+					if (b(pl, "entitymanager.interact.potion." + b.getNameId())) {
+						return;
+					}
+					if (c.get(6).contains(b.getNameId())) {
+						if (b(e.getClickedBlock())) {
+							e.setUseItemInHand(Result.DENY);
+							return;
+						} else {
+							e.setUseItemInHand(Result.DENY);
+							e.setCancelled(true);
+							al(c, "Player " + pl.getName()
+									+ " tried to use an " + c(b) + " potion"
+									+ ".");
+							al(c, pl,
+									"&cYou don't have permission to use that &6"
+											+ c(b) + " potion" + "&c.");
+						}
+					}
+					return;
+				}
+				if (!b(pl, "entitymanager.interact." + str)) {
+					String item = str.replace("_", " ");
+					if (b(e.getClickedBlock())) {
+						e.setUseItemInHand(Result.DENY);
+						return;
+					} else {
+						e.setUseItemInHand(Result.DENY);
+						e.setCancelled(true);
+						al(c, "Player " + pl.getName() + " tried to use an "
+								+ item + ".");
+						al(c, pl, "&cYou don't have permission to use that &6"
+								+ item + "&c.");
+					}
+				}
 			}
 		}
 	}
