e8c143863a6ef0623c67421a182d7194f107a0e0
Nick Porillo <milkywayz@mail.com>
2013-12-19 23:10:58 -0500

    Removed useless utility method

--- src/net/milkycraft/Utility.java
+++ src/net/milkycraft/Utility.java
@@ -16,7 +16,6 @@ import org.bukkit.entity.Player;
 import org.bukkit.entity.Sheep;
 import org.bukkit.event.Listener;
 import org.bukkit.potion.Potion;
-import org.bukkit.potion.PotionType;
 
 public abstract class Utility {
 
@@ -121,24 +120,5 @@ public abstract class Utility {
 					"No config found for " + world + ", generating one now");
 			return manager.load(world);
 		}
-	}
-
-	public static Potion fromDamage(int damage) {
-		PotionType type = PotionType.getByDamageValue(damage & 0xF);
-		Potion potion;
-		if (type == null || (type == PotionType.WATER && damage != 0)) {
-			potion = new Potion(damage & 0x3F);
-		} else {
-			int level = (damage & 0x20) >> 5;
-			level++;
-			potion = new Potion(type, level);
-		}
-		if ((damage & 0x4000) > 0) {
-			potion = potion.splash();
-		}
-		if ((damage & 0x40) > 0) {
-			potion = potion.extend();
-		}
-		return potion;
 	}
 }
