80bf50f9027fdad50d931a2ff1314cdc0ea5b1fe
Nick Porillo <milkywayz@mail.com>
2014-01-05 17:40:34 -0500

    Remove unnecessary code

--- src/net/milkycraft/objects/Item.java
+++ src/net/milkycraft/objects/Item.java
@@ -1,36 +1,24 @@
 package net.milkycraft.objects;
 
-import org.bukkit.Material;
-
 public class Item {
 
 	private int id;
-	private int potid;
-	private boolean isPot;
+	private int dura;
 
-	public Item(int id, int potid, boolean isPot) {
+	public Item(int id, int dura) {
 		this.id = id;
-		this.potid = potid;
-		this.isPot = isPot;
+		this.dura = dura;
 	}
 
 	public Item(int id) {
-		this(id, Integer.MAX_VALUE, false);
-	}
-
-	public boolean isPotion() {
-		return isPot;
+		this(id, 0);
 	}
 
 	public int getId() {
 		return id;
 	}
 
-	public int getNameId() {
-		return potid;
-	}
-
-	public String getName() {
-		return Material.getMaterial(id).toString();
+	public int getDurability() {
+		return dura;
 	}
 }
