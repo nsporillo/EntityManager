1e577c3b4c67e92e4e7128c1ff5948ae095d158c
Nick Porillo <milkywayz@mail.com>
2014-07-18 23:44:46 -0400

    Code organization & Config update changes
    When a really out of date config is loaded on a recent EM, it'll be
    passed through the update process several times. Therefore only the
    latest "successfully updated..." message is displayed :)

--- src/net/milkycraft/config/WorldConfiguration.java
+++ src/net/milkycraft/config/WorldConfiguration.java
@@ -19,7 +19,7 @@ import org.bukkit.event.entity.CreatureSpawnEvent.SpawnReason;
 public class WorldConfiguration extends ConfigLoader {
 
 	private final String world;
-	private final double version = 1.7;
+	private final double REV = 1.7;
 	private boolean[] b = new boolean[23];
 	private long[] l = new long[2];
 	protected Set<Item> usageBlock = new HashSet<Item>();
@@ -41,35 +41,30 @@ public class WorldConfiguration extends ConfigLoader {
 		if (performUpdate(c.getDouble("Settings.Config_Revision", 0.1))) {
 			l[0] = c.getLong("TimeManager.Target_Time", 12000L);
 			l[1] = c.getLong("TimeManager.Set_Every", 100L);
-			b[0] = c.getBoolean("Settings.Admin-Alerts", true);
-			b[1] = c.getBoolean("Settings.Player-Alerts", true);
-			b[2] = c.getBoolean("Settings.Logging", true);
-			b[3] = c.getBoolean("Disable.Weather.Rain", false);
-			b[4] = c.getBoolean("Disable.Weather.Thunder", false);
-			b[5] = c.getBoolean("Disable.Weather.Lightning", false);
-			b[6] = c.getBoolean("Disable.Interaction.PVP", false);
-			b[7] = c.getBoolean("Disable.Interaction.Fishing", false);
-			b[8] = c.getBoolean("Disable.Interaction.Shooting", false);
-			b[9] = c.getBoolean("Disable.Interaction.Enchanting", false);
-			b[10] = c.getBoolean("Disable.Interaction.Fireworks", false);
-			b[11] = c.getBoolean("Disable.Interaction.Trading", false);
-			b[12] = c.getBoolean("Disable.Other.Monster_Spawner_Exp", false);
-			b[13] = c.getBoolean("Disable.Other.Monster_Spawner_Drops", false);
-			b[14] = c.getBoolean("TimeManager.Enabled", false);
-			b[15] = c.getBoolean("EggManager.Disable_All", false);
-			b[16] = c.getBoolean("SpawnManager.Disallow_All", false);
-			b[17] = c.getBoolean("SpawnManager.Remove_Armor", false);
-			b[18] = c.getBoolean("DeathManager.Player.Keep_Exp", false);
-			b[19] = c.getBoolean("DeathManager.Entity.Drop_Exp", true);
-			b[20] = c.getBoolean("DeathManager.Entity.Drop_Items", true);
-			b[21] = c.getBoolean("DeathManager.Player.Keep_Items", false);
-			b[22] = c.getBoolean("Disable.Interaction.Portal_Creation", false);
-			ConfigUtility.loadBlockedItems(this);
-			ConfigUtility.loadBlockedDispenserItems(this);
-			ConfigUtility.loadBlockedInteractionBlocks(this);
-			ConfigUtility.loadBlockedSpawnEggs(this);
-			ConfigUtility.loadBlockedEntities(this);
-			ConfigUtility.loadBlockedSpawnReasons(this);
+			load(0, "Settings.Admin-Alerts", true);
+			load(1, "Settings.Player-Alerts", true);
+			load(2, "Settings.Logging", true);
+			load(3, "Disable.Weather.Rain", false);
+			load(4, "Disable.Weather.Thunder", false);
+			load(5, "Disable.Weather.Lightning", false);
+			load(6, "Disable.Interaction.PVP", false);
+			load(7, "Disable.Interaction.Fishing", false);
+			load(8, "Disable.Interaction.Shooting", false);
+			load(9, "Disable.Interaction.Enchanting", false);
+			load(10, "Disable.Interaction.Fireworks", false);
+			load(11, "Disable.Interaction.Trading", false);
+			load(12, "Disable.Other.Monster_Spawner_Exp", false);
+			load(13, "Disable.Other.Monster_Spawner_Drops", false);
+			load(14, "TimeManager.Enabled", false);
+			load(15, "EggManager.Disable_All", false);
+			load(16, "SpawnManager.Disallow_All", false);
+			load(17, "SpawnManager.Remove_Armor", false);
+			load(18, "DeathManager.Player.Keep_Exp", false);
+			load(19, "DeathManager.Entity.Drop_Exp", false);
+			load(20, "DeathManager.Entity.Drop_Items", true);
+			load(21, "DeathManager.Player.Keep_Items", false);
+			load(22, "Disable.Interaction.Portal_Creation", false);
+			ConfigUtility.load(this);
 		} else {
 			super.saveConfig();
 			super.rereadFromDisk();
@@ -77,9 +72,17 @@ public class WorldConfiguration extends ConfigLoader {
 		}
 	}
 
-	public boolean performUpdate(double rev) {
+	private void load(int i, String sec, boolean def) {
+		b[i] = c.getBoolean(sec, def);
+	}
+
+	public Logger getLog() {
+		return plugin.getLogger();
+	}
+
+	private boolean performUpdate(double rev) {
 		Logger lg = plugin.getLogger();
-		if (this.version == rev) {
+		if (this.REV == rev) {
 			return true;
 		}
 		if (rev == 0.1) {
@@ -87,19 +90,16 @@ public class WorldConfiguration extends ConfigLoader {
 			super.set("Disable.Usage.Potions", false);
 			super.set("Disable.Usage.Splash_Potions", false);
 			super.set("Disable.Interaction.Trading", false);
-			lg.info("Successfully updated " + fileName + " to 1.0");
 			return false;
 		} else if (rev == 1.0) {
 			super.set("Settings.Config_Revision", 1.1);
 			List<String> list = new ArrayList<String>();
 			list.add(SpawnReason.LIGHTNING.toString().toLowerCase());
 			super.set("SpawnManager.Disallowed_Reasons", list);
-			lg.info("Successfully updated " + fileName + " to 1.1");
 			return false;
 		} else if (rev == 1.1) {
 			super.set("Settings.Config_Revision", 1.2);
 			super.set("DeathManager.Keep_Exp", false);
-			lg.info("Successfully updated " + fileName + " to 1.2");
 			return false;
 		} else if (rev == 1.2) {
 			super.set("Settings.Config_Revision", 1.3);
@@ -107,7 +107,6 @@ public class WorldConfiguration extends ConfigLoader {
 			super.set("DeathManager.Player.Keep_Exp", false);
 			super.set("DeathManager.Entity.Drop_Exp", true);
 			super.set("DeathManager.Entity.Drop_Items", true);
-			lg.info("Successfully updated " + fileName + " to 1.3");
 			return false;
 		} else if (rev == 1.3) {
 			super.set("Settings.Config_Revision", 1.4);
@@ -115,7 +114,6 @@ public class WorldConfiguration extends ConfigLoader {
 			List<String> list = c.getStringList("Usage.Blocked_Items");
 			list.add("Potion:16394");
 			super.set("Disable.Usage.Blocked_Items", list);
-			lg.info("Successfully updated " + fileName + " to 1.4");
 			return false;
 		} else if (rev == 1.4) {
 			super.set("Settings.Config_Revision", 1.5);
@@ -124,12 +122,10 @@ public class WorldConfiguration extends ConfigLoader {
 			List<String> list = c.getStringList("SpawnManager.Disallowed_Mobs");
 			list.add("Zombie:baby:villager");
 			super.set("SpawnManager.Disallowed_Mobs", list);
-			lg.info("Successfully updated " + fileName + " to 1.5");
 			return false;
 		} else if (rev == 1.5) {
 			super.set("Settings.Config_Revision", 1.6);
 			super.set("Disable.Interaction.Portal_Creation", false);
-			lg.info("Successfully updated " + fileName + " to 1.6");
 			return false;
 		} else if (rev == 1.6) {
 			super.set("Settings.Config_Revision", 1.7);
@@ -138,9 +134,8 @@ public class WorldConfiguration extends ConfigLoader {
 			lg.info("Successfully updated " + fileName + " to 1.7");
 			return false;
 		} else {
-			lg.warning("Could not update config, mismatched config revision: Local: " + version
-					+ " File: " + rev);
-			return false;
+			lg.warning("Config update failed, unknown version {L:" + REV + "..." + rev + "?}");
+			return true;
 		}
 	}
 
@@ -195,21 +190,21 @@ public class WorldConfiguration extends ConfigLoader {
 		return false;
 	}
 
-	public boolean dispense(int id) {
-		for (Item i : this.dispBlock) {
-			if (i.getId() == id) {
-				return true;
-			}
-		}
-		return false;
-	}
-
 	public boolean dispensePotion(int pot) {
 		for (Item i : this.dispBlock) {
 			if (i.getId() == 373) {
 				if (i.getDurability() == pot) {
 					return true;
 				}
+			}
+		}
+		return false;
+	}
+
+	public boolean dispense(int id) {
+		for (Item i : this.dispBlock) {
+			if (i.getId() == id) {
+				return true;
 			}
 		}
 		return false;
